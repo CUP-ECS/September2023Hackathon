2cdb0c5c62e65ebc394419054efd41adb18bea89
Derek Schafer <schaferderek96@gmail.com>
2024-01-18 07:15:12 -0800

    Updated extraction tool

--- extract_data.py
+++ extract_data.py
@@ -12,26 +12,30 @@ def main():
                 with open(entry, 'r') as file:
                     print_string =""
                     data = file.read()
-                    nodes, data = data.split("Modules")
-                    nodes = nodes.split(":")[1].strip()
+                    if(machine == "lassen"):
+                        nodes, data, _ = data.split("Modules")
+                        nodes = nodes.split(":")[-1].strip()
+                    else:
+                        nodes, data = data.split("Modules")
+                        nodes = nodes.split(":")[1].strip()
                     MPI, data = data.split("\n",1)
                     MPI = MPI.lstrip(': ')
                     for matrix in data.split("Starting ")[1:]:
                         matrix_name, matrix = matrix.split("\n",1)
                         matrix_name.strip()
-                        form_time = ""
-                        p2p_time  = ""
-                        mpi_time  = ""
-                        mpix_time = ""
+                        form_time = 0.0
+                        p2p_time  = 0.0
+                        mpi_time  = 0.0
+                        mpix_time = 0.0
                         for line in matrix.split("\n"):
                             if "form: " in line:
-                                form_time = line.rstrip().split(": ")[1]
+                                form_time += float(line.rstrip().split(": ")[1])
                             elif "Standard comm:" in line:
-                                p2p_time = line.rstrip().split(": ")[1]
+                                p2p_time += float(line.rstrip().split(": ")[1])
                             elif "Standard graph" in line or "Standard neighbor: " in line:
-                                mpi_time = line.rstrip().split(": ")[1]
+                                mpi_time += float(line.rstrip().split(": ")[1])
                             elif "advance" in line:
-                                mpix_time = line.rstrip().split(": ")[1]
+                                mpix_time += float(line.rstrip().split(": ")[1])
 
                         print(machine,nodes,matrix_name,MPI,experiment,p2p_time,mpi_time,mpix_time,sep=",")
                     print(print_string, end="")
